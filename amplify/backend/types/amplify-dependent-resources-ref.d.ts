ab1d9d031edd500fb1b299ce1063a822ab8aae6e
Umoren <samuelumoren365@gmail.com>
2022-11-21 09:15:39 +0100

    initil commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "nextimagedemo30faf403": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "amplifystore": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    images: {
+      domains: ["nextimagedemo1112419-dev.s3.amazonaws.com"]
+    }
+
+  },
+}
